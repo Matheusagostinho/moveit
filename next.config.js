69b2083aa8d49917aad06d677b1ac3918c3d2eeb
Matheus Agostinho <henrique.matheus28@gmail.com>
2021-02-28 19:29:30 -0300

    Add fallback

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    swSrc: 'service-worker.js',
-    runtimeCaching
+    swSrc: 'service-worker.js'
   }
 })
