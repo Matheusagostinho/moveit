151ec6da3767498f16c2daba5efc723fb0882595
Matheus Agostinho <henrique.matheus28@gmail.com>
2021-02-28 18:50:05 -0300

    Add fallback

---
+++ src/pages/fallback.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/display-name */
+import { Main } from 'containers/Main'
+import Head from 'next/head'
+
+export default () => (
+  <>
+    <Head>
+      <title>Moveit | Off-line</title>
+    </Head>
+    <Main></Main>
+  </>
+)
