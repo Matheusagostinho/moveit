e169c85fe1044013d80d0d9d1e744d630888021d
Matheus Agostinho <henrique.matheus28@gmail.com>
2021-03-02 16:51:03 -0300

    Add Animation NProgress

--- src/containers/LogInBG/index.tsx
+++ src/containers/LogInBG/index.tsx
@@ -13,22 +13,6 @@ export function LogInBG() {
         <header className="pageFadeIn">
           <img src="/images/Logo.svg" alt="" />
         </header>
-
-        <main className="pageFadeIn">
-          <strong>Seja Bem-vindo</strong>
-          <p>Faça login para continuar</p>
-        </main>
-
-        <div className="pageFadeIn">
-        <button className="pageFadeIn" onClick={() => signIn('github') }>
-            {/* <img src="/icons/Github.svg" alt="github logo" /> */}
-            SignIn with GitHub
-          </button>
-          <button className="pageFadeIn" onClick={() => signIn('auth0') }>
-            SignIn with Email
-          </button>
-
-        </div>
       </div>
     </S.Container>
   )
