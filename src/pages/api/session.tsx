7e0999550cfaa3be510d44e596a0bfccc0c84271
Matheus Agostinho <henrique.matheus28@gmail.com>
2021-03-02 00:11:39 -0300

    Error

--- src/pages/api/session.tsx
+++
@@ -1,9 +0,0 @@
-import { setOptions, getSession } from 'next-auth/client'
-import {SetOptionsParams} from 'next-auth/client'
-setOptions({ site: process.env.NEXTAUTH_URL } as SetOptionsParams)
-
-export default async (req, res) =>  {
-  const session = await getSession({ req })
-  console.log('session', session)
-  res.end()
-}
